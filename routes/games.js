dc66dda0b72326d8af294d121d30291993893319
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-09-15 12:38:35 +0200

    created new.ejs view, added router for the new endpoint and reinstalled vue.js

--- routes/games.js
+++ routes/games.js
@@ -5,6 +5,13 @@ const router = express.Router();
 router.get('/', (req, res) => {
   console.log('requesting root page');
   res.send('<h1>Root Page</h1>');
-})
+});
+
+// GET request for the "new game" page
+router.get('/new', (req, res) => {
+  console.log('Requesting new page');  
+  // res.send('<h1>test</h1>');
+  res.render('games/new');
+});
 
 module.exports = router;
